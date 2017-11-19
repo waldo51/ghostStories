3c902bfde864b49389f2edc00bd0d21d08a40718
Warren Sutherland <w.sutherland8@gmail.com>
2017-11-18 22:05:26 -0700

    Nav and basic templates

--- GhostStories/www/js/routes.js
+++ GhostStories/www/js/routes.js
@@ -9,15 +9,34 @@
 
       $urlRouterProvider.otherwise('welcome');
 
+
+      //TODO: Do I Need any of these controllers as all are the same and included in index.html?
       $stateProvider
         .state('welcome', {
           url: '/welcome',
-          views: {
-            settings: {
-              controller: 'DataController',
-              templateUrl: 'js/templates/welcome.html'
-            }
-          }
-        });
+          controller: 'DataController',
+          templateUrl: 'js/templates/welcome.html'
+        })
+        .state('dataEntry1', {
+          url: '/dataentry',
+          controller: 'DataController',
+          templateUrl: 'js/templates/data-entry1.html'
+        })
+        .state('dataEntry2', {
+          url: '/dataentry',
+          controller: 'DataController',
+          templateUrl: 'js/templates/data-entry2.html'
+        })
+        .state('saved', {
+          url: '/saved',
+          controller: 'DataController',
+          templateUrl: 'js/templates/saved.html'
+        })
+        .state('scoreboard', {
+          url: '/scoreboard',
+          controller: 'DataController',
+          templateUrl: 'js/templates/scoreboard.html'
+        })
+        ;
     }
   })();
